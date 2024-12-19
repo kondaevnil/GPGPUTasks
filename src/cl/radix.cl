0123e540fd7ca07a513aa36e2d64b795588779f1
kondaevnil <defox4159@gmail.com>
2024-12-19 17:13:34 +0300

    add: task 8.1

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,1 +1,62 @@
-// TODO
+#ifdef __CLION_IDE__
+#include "clion_defines.cl"
+#endif
+
+#line 5
+
+#define WGS 64
+#define BTS 4
+
+__kernel void radix(__global const uint *a, __global uint *b, __global uint *counters, __global uint *pref, uint shift) {
+    uint i = get_global_id(0);
+
+    __local uint local_counters[1 << BTS];
+    for (int j = 0; j < (1 << BTS); j++) {
+        local_counters[j] = 0;
+    }
+
+    for (int j = 0; j < WGS; j++) {
+        uint index = i * WGS + j;
+        uint value = (a[index] >> shift) & 0xF;
+        uint p = value * get_num_groups(0) + get_group_id(0);
+        uint pos = pref[p] - counters[p] + local_counters[value];
+        local_counters[value]++;
+        b[pos] = a[index];
+    }
+}
+
+__kernel void counters(__global const uint *a, __global uint *counters, uint shift) {
+    __local uint local_counters[WGS];
+    uint i = get_local_id(0);
+    local_counters[i] = 0;
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    uint index = get_global_id(0);
+
+    uint value = (a[index] >> shift) & 0xF;
+    atomic_inc(&local_counters[value]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (i < (1 << BTS)) {
+        counters[i * get_num_groups(0) + get_group_id(0)] = local_counters[i];
+    }
+}
+
+__kernel void copy_arrays(__global const uint *a, __global uint *b, uint n) {
+    uint i = get_global_id(0);
+    if (i < n) {
+        b[i] = a[i];
+    }
+}
+
+__kernel void prefix_bin(__global uint *a, uint stride, uint n) {
+   uint i = (get_global_id(0) + 1) * stride - 1;
+   if (i < n)
+        a[i] += a[i - stride / 2];
+}
+
+__kernel void prefix(__global uint *pref, uint stride, uint n) {
+    uint i = (get_global_id(0) + 1) * stride - 1 + stride / 2;
+    if (i < n)
+        pref[i] += pref[i - stride / 2];
+}
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -24,6 +24,9 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 }
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
+#define WGS 64
+#define VPW 32
+#define BTS 4
 
 std::vector<unsigned int> computeCPU(const std::vector<unsigned int> &as)
 {
@@ -58,21 +61,73 @@ int main(int argc, char **argv) {
 
     const std::vector<unsigned int> cpu_reference = computeCPU(as);
 
-    // remove me
-    return 0;
+    gpu::gpu_mem_32u as_gpu;
+    as_gpu.resizeN(n);
+    gpu::gpu_mem_32u bs_gpu;
+    bs_gpu.resizeN(n);
+
+    gpu::gpu_mem_32u counters;
+    uint counters_size = (1 << BTS) * (n / WGS);
+    counters.resizeN(counters_size);
+
+    gpu::gpu_mem_32u pref;
+    pref.resizeN(counters_size);
 
     {
+        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
+        radix.compile();
+
+        ocl::Kernel count(radix_kernel, radix_kernel_length, "counters");
+        count.compile();
+
+        ocl::Kernel prefix(radix_kernel, radix_kernel_length, "prefix");
+        prefix.compile();
+
+        ocl::Kernel prefix_bin(radix_kernel, radix_kernel_length, "prefix_bin");
+        prefix_bin.compile();
+
+        ocl::Kernel copy(radix_kernel, radix_kernel_length, "copy_arrays");
+        copy.compile();
+
+        std::vector<unsigned int> cnt(counters_size, 0);
+        std::vector<unsigned int> prf(counters_size, 0);
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
+            as_gpu.writeN(as.data(), as.size());
+            t.restart();
 
-            // TODO
+            for (int shift = 0; shift < 32; shift += BTS) {
+                // counter
+                count.exec(gpu::WorkSize(WGS, n), as_gpu, counters, shift);
+                counters.readN(cnt.data(), counters_size);
+
+                copy.exec(gpu::WorkSize(WGS, n), counters, pref, counters);
+
+                // pref sums
+                for (unsigned int stride = 2; stride <= counters_size; stride *= 2) {
+                    prefix_bin.exec(gpu::WorkSize(8, counters_size / stride), pref, stride, counters_size);
+                }
+
+                for (unsigned int stride = counters_size / 2; stride >= 2; stride /= 2) {
+                    prefix.exec(gpu::WorkSize(8, (counters_size + stride - 1) / stride), pref, stride, counters_size);
+                }
+                pref.readN(prf.data(), counters_size);
+
+                // sort
+                radix.exec(gpu::WorkSize(1, (n + WGS - 1) / WGS), as_gpu, bs_gpu, counters, pref, shift);
+
+                std::swap(as_gpu, bs_gpu);
+            }
+
+            t.nextLap();
         }
-        t.stop();
 
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
+
+    as_gpu.readN(as.data(), n);
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
